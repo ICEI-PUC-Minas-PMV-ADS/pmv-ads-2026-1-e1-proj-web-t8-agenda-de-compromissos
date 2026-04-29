09c4b6a8eda7f3de5718c69b14d9df16cc779315
lucaslamarca <lucasfelipe0411@gmail.com>
2026-04-28 23:19:54 -0300

    register.js-version.0001
    Tela de cadastro de usuários versão 01 script

---
+++ codigo-fonte/js/register.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('cadastroUser');
+
+//evento de envio de formulario 
+form.addEventListener('submit', function(event){
+//evita recarregar a pagina
+event.preventDefault();
+//pegando os valores dos campos
+const nome = document.getElementById('nome').value;
+const email = document.getElementById('email').value;
+const dataN = document.getElementById('dataN').value;
+const tel = document.getElementById('tel').value;
+const senha = document.getElementById('senha').value;
+
+//criando objeto usuario
+const usuario = {
+    nome,
+    email,
+    dataN,
+    tel,
+    senha
+};
+
+//pegando usuario ja cadastrado
+let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+//verificando se o email ja existe
+const emailExiste = usuarios.some(user => user.email === email);
+
+if(emailExiste){
+    document.getElementById('mensageMM').innerHTML = 'email ja cadastrado';
+    document.getElementById('mensageMM').style.color = 'red';
+return;
+}
+
+//adicionar novo usuario
+usuarios.push(usuario);
+
+//salvando no localStorage
+localStorage.setItem(usuarios, JSON.stringify(usuarios));
+
+//mensagem de sucesso
+document.getElementById('mensagem').innerHTML = 'cadastro realizado com sucesso';
+document.getElementById('mensagem').style.color = 'green';
+
+//limpar formulario
+form.reset(); 
+
+});
