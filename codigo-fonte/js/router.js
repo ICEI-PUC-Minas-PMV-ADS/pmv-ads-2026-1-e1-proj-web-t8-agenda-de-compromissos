f29f450de352e5080b29fce238fbe41b86b5105b
Pablo Luiz Amaro de Miranda <pablo.profissional.de.ti@gmail.com>
2026-06-04 00:15:10 -0300

    router.js-version01

---
+++ codigo-fonte/js/router.js
@@ -0,0 +1,6 @@
+(function checkAuthentication() {
+    const user = localStorage.getItem('agenda_current_user');
+    if (!user && !window.location.pathname.endsWith('index.html') && !window.location.pathname.endsWith('register_user.html')) {
+        window.location.href = '../index.html';
+    }
+})();
